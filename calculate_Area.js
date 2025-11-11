4c7ef94d18c834eac0ae33669f7a3f9352b23fb4
abhinavANAND13 <abhinavanand1310@gmail.com>
2025-11-11 14:46:59 -0500

    message

--- calculate_Area.js
+++ calculate_Area.js
@@ -9,3 +9,17 @@ let area = length * width;
 
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let g1;
+let g2;
+let g3;
+
+function groceryTracker() {
+ g1 = parseFloat(document.getElementById('g1').value);
+ g2 = parseFloat(document.getElementById('g2').value);
+ g3 = parseFloat(document.getElementById('g3').value);
+
+let area = g1 + g2 +g3;
+
+ document.getElementById('resultG').innerText = `The Total Amount:$ ${area}`;
+}
